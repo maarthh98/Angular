06324cba4011d9b0ccf248de670e1232ca290d9b
maarthh98 <maarthh98@gmail.com>
2020-07-03 20:22:36 +0200

    lesson1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {User} from './models/User';
+import {Post} from './models/Post';
+import {Comment} from './models/Comment';
+
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+
+  template: '<h1>HELLO</h1> ' +
+    '<app-user *ngFor="let u of users"[userX]="u"></app-user>' +
+    '<div><app-post *ngFor="let p of posts" [post]="p"></app-post></div>'+
+    '<div><app-comments *ngFor="let c of comments"[comments]></app-comments></div>',
+
+  styles: ['h1{background-color: cadetblue}', 'div{margin-bottom: 10px; border: 1px solid black}']
 })
 export class AppComponent {
-  title = 'lesson1angular';
+
+  users: User[];
+  posts: Post[];
+  comments: Comment[];
+
+ constructor(private httpClient: HttpClient) {
+    this.httpClient
+      .get<User[]>('https://jsonplaceholder.typicode.com/users')
+      .subscribe(data => this.users = data)
+    ;
+   this.httpClient
+     .get<Post[]>('https://jsonplaceholder.typicode.com/posts')
+     .subscribe(data => this.posts = data)
+   ;
+   this.httpClient
+     .get<any[]>('https://jsonplaceholder.typicode.com/comments')
+     .subscribe(data => this.comments = data)
+   ;
+ }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+import { UserComponent } from './components/user/user.component';
+import { PostComponent } from './components/post/post.component';
+import { CommentComponent } from './components/comment/comment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    PostComponent,
+    CommentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
